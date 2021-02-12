a6372689d310809f66d811d6186273f32d2795b1
Justin Knueppel <justinknueppel@gmail.com>
2021-02-12 08:34:04 -0500

    Allow command line arguments

--- generate-config.py
+++ generate-config.py
@@ -1,7 +1,17 @@
+from argparse import ArgumentParser
 from piawg import piawg
 from pick import pick
 from getpass import getpass
 from datetime import datetime
+
+# Get commandline arguments
+parser = ArgumentParser(description="Wireguard configuration generator")
+parser.add_argument("-u", "--username", type=str, metavar="USER", dest="username", default="")
+parser.add_argument("-p", "--password", type=str, metavar="PASS", dest="password", default="")
+parser.add_argument("-r", "--region", type=str, metavar="REG", dest="region")
+parser.add_argument("-o", "--out", type=str, metavar="OUT", dest="out")
+
+args = parser.parse_args()
 
 pia = piawg()
 
@@ -9,21 +19,27 @@ pia = piawg()
 pia.generate_keys()
 
 # Select region
-title = 'Please choose a region: '
-options = sorted(list(pia.server_list.keys()))
-option, index = pick(options, title)
-pia.set_region(option)
-print("Selected '{}'".format(option))
+if args.region and args.region in pia.server_list.keys():
+    pia.set_region(args.region)
+else:
+    title = 'Please choose a region: '
+    options = sorted(list(pia.server_list.keys()))
+    option, index = pick(options, title)
+    pia.set_region(option)
+    print("Selected '{}'".format(option))
 
 # Get token
-while True:
-    username = input("\nEnter PIA username: ")
-    password = getpass()
-    if pia.get_token(username, password):
-        print("Login successful!")
-        break
-    else:
-        print("Error logging in, please try again...")
+if pia.get_token(args.username, args.password):
+    print("Login successful!")
+else:
+    while True:
+        username = input("\nEnter PIA username: ")
+        password = getpass()
+        if pia.get_token(username, password):
+            print("Login successful!")
+            break
+        else:
+            print("Error logging in, please try again...")
 
 # Add key
 status, response = pia.addkey()
@@ -36,7 +52,7 @@ else:
 # Build config
 timestamp = int(datetime.now().timestamp())
 location = pia.region.replace(' ', '-')
-config_file = 'PIA-{}-{}.conf'.format(location, timestamp)
+config_file = args.out if args.out else 'PIA-{}-{}.conf'.format(location, timestamp)
 print("Saving configuration file {}".format(config_file))
 with open(config_file, 'w') as file:
     file.write('[Interface]\n')
